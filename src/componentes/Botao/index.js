f235f48d8ad284c828120fa0e640b9e9c5879693
Israel Humberto <israelhumberto99@gmail.com>
2022-07-18 08:21:48 -0300

    add components button and dropdown

---
+++ src/componentes/Botao/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Botao.css'
+
+const Botao = (props) => {
+  return (
+    <button className='btn'>{props.children}</button>
+  )
+}
+
+export default Botao
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,24 +1,49 @@
 import React from 'react'
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
+
+  const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Mobile',
+    'Devops',
+    'UX e Design'
+  ]
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault()
+    console.log('Form foi salvo')
+  }
+
   return (
     <section className='formulario'>
-        <form>
+        <form onSubmit={aoSalvar}>
             <h2>Preencha seus dados</h2>
             <CampoTexto 
             label='Nome'
             placeholder='Digite seu nome'
+            required={true}
             />
             <CampoTexto 
             label='Cargo'
             placeholder='Digite seu cargo'
+            required={true}
             />
             <CampoTexto 
             label='Imagem'
             placeholder='Insira a url da sua foto'
+            required={true}
             />
+            <ListaSuspensa 
+            label='Time'
+            itens = {times}
+            />
+            <Botao>Criar Card</Botao>
         </form>
     </section>
   )
